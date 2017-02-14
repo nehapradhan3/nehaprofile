a14ca61cc78819b52ac77da9716e39459d26de72
nehapradhan <nehapradhan_3@hotmail.com>
2017-02-14 23:09:25 +0545

    notes memos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
-
+import {NotesComponent} from'./notes/notes.component';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+
 })
 export class AppComponent {
   title = 'app works!';
